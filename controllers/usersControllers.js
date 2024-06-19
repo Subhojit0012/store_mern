307dbe7faabdeb2ae4b36123edc4d8db77a0ac10
Subhojit0012 <subhojitroy00012@gmail.com>
2024-06-19 20:16:32 +0530

    database connection setup ✅

---
+++ controllers/usersControllers.js
@@ -0,0 +1,4 @@
+const User = require("../models/User");
+const Note = require("../models/Note");
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
--- server.js
+++ server.js
@@ -2,22 +2,26 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const app = express();
-const { logger } = require("./middleware/logger");
 const port = process.env.PORT || 3500;
 const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
+const connectDB = require("./DataBase/dbConnect");
+const mongoose = require("mongoose");
+const { logEvents, logger } = require("./middleware/logger");
 
 app.use(logger);
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
+connectDB();
 
 // app.use("/", express.static(path.join(__dirname, "public"))); //✅
 app.use(express.static("public")); // ✅
 
 app.use("/", require("./routes/root"));
+app.use("/user", require("./routes/userRoutes"));
 
 app.all("*", (req, res) => {
   res.status(404);
@@ -32,6 +36,16 @@ app.all("*", (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(port, () => {
-  console.log("⚙️  Server is running at: http://localhost:%d", port);
+mongoose.connection.once("open", () => {
+  app.listen(port, () => {
+    console.log("⚙️  Server is running at: http://localhost:%d", port);
+  });
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErr.log"
+  );
 });
