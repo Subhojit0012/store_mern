b503449ab8c803f797e5ab416f1114536b16d526
Subhojit0012 <subhojitroy00012@gmail.com>
2024-10-10 09:59:21 +0530

    edit user

--- store/src/features/users/EditUser.js
+++ store/src/features/users/EditUser.js
@@ -1,9 +1,15 @@
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectUserById } from "./userApiSlice";
+import EditUserForm from "./EditUserForm";
 const EditUser = () => {
-    return ( 
-    <div>
-        Edit user
-    </div> 
-    );
-}
- 
+  const { id } = useParams();
+
+  const user = useSelector((state) => selectUserById(state, id));
+
+  const content = user ? <EditUserForm user={user} /> : <p>loading...</p>;
+
+  return content;
+};
+
 export default EditUser;
