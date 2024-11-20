b29a061b2d68a0c692309e00cf1128e034fad696
Subhojit0012 <subhojitroy00012@gmail.com>
2024-11-20 19:34:28 +0530

    apply JWT and verify JWT

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const noteControllers = require("../controllers/noteControllers");
+const verifyJWT = require("../middleware/verifyJWT");
+
+router.use(verifyJWT);
 
 router
   .route("/")
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const usersControllers = require("../controllers/usersControllers");
+const verifyJWT = require("../middleware/verifyJWT");
+
+router.use(verifyJWT);
 
 router
   .route("/")
