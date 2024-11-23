c4fa460a81c200074f1fb1dca9dbd40b72710f34
Subhojit0012 <subhojitroy00012@gmail.com>
2024-11-23 23:18:33 +0530

    bug resolved

--- store/src/app/api/apiSlice.js
+++ store/src/app/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'http"//localhost:8080',
+  baseUrl: 'http://localhost:8080',
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
